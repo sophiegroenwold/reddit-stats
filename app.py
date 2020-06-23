5426ed15ce7995187c94ae3050999efe7c692f61
sophiegroenwold <sophiegroenwold@umail.ucsb.edu>
2020-06-22 23:03:50 -0700

    layout of left side, stuck on input query for subreddit

--- app.py
+++ app.py
@@ -1,43 +1,95 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Output, State, Input
 
 import pandas as pd
 
 import data
 
-hot_df = data.hot()
-
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# def generate_list(dataframe, max_rows=10):
-#     return html.Table([
-#         html.Tbody([
-#             html.Tr([
-#                 html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-#             ]) for i in range(min(len(dataframe), max_rows))
-#         ])
-#     ])
-
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
 
 def generate_list(dataframe):
     return html.Div([
         html.A(dataframe.iloc[i][0], href = dataframe.iloc[i][1], className = "newline", target = "_blank") for i in range(len(dataframe))
     ])
 
-app.layout = html.Div(children=[
-    html.H1(children='Reddit Statistics'),
+app.layout = html.Div([
+    # represents the URL bar, doesn't render anything
+    dcc.Location(id='url', refresh=False),
 
-    # html.Div(children='''
-    #     Dash: A web application framework for Python.
-    # '''),
+    html.H1(children='redditometer'),
+    html.Div('Enter the name of a subreddit to view stats and toxicity.'),
 
-    # hot posts
-    generate_list(hot_df)
+    # for link
+    # dcc.Link('Navigate to /UCSantaBarbara', href='/UCSantaBarbara'),
+    # html.Div(id='page-content')
+
+    # for input box
+    # dcc.Input(id='input_subreddit_id', type='text'),
+    # html.Div(id='input_subreddit_div')
 ])
+
+# for link
+# @app.callback(dash.dependencies.Output('page-content', 'children'),
+#               [dash.dependencies.Input('url', 'pathname')])
+
+# for input box
+# @app.callback(
+#     Output(component_id='input_subreddit_div', component_property='children'),
+#     [Input(component_id='input_subreddit_id', component_property='value')]
+# )
+
+def display_page(pathname):
+    hot_df = data.hot('UCSantaBarbara')
+    new_df = data.new('UCSantaBarbara')
+    name = data.name('UCSantaBarbara')
+    description = data.description('UCSantaBarbara')
+    num_subscribers = data.num_subscribers('UCSantaBarbara')
+    time_created = data.time_created('UCSantaBarbara')
+    top_users_day_df = data.top_users('UCSantaBarbara', 'day')
+
+    if pathname == '/':
+        return html.Div('')
+
+    else:
+        return html.Div(children=[
+            html.Div(className = "parent_left", children = 
+                html.Div(className = "quick_stats", children=[
+                    html.H2('quick stats'),
+
+                    # subreddit basic info
+                    html.Div('Welcome to ', className = 'div_no_new_line'),
+                    html.A('r/' + name, href = 'https://www.reddit.com/r/' + name, className = "no_newline", target = "_blank"),
+                    html.Div(description),
+                    html.Div(str(num_subscribers) + ' subscribers'),
+                    html.Div('Created ' + time_created),
+
+                    # hot posts
+                    html.H3('hot posts'),
+                    # generate_list(hot_df),
+
+                    # top posts
+                    html.H3('new posts'),
+                    generate_list(new_df), 
+                    
+                    # top users of the day
+                    html.H3('top users'),
+                    generate_table(top_users_day_df)
+                ])
+            )
+        ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- data.py
+++ data.py
@@ -1,12 +1,35 @@
-import praw
 from secrets import mySecrets
 
+import praw
 import pandas as pd
+from datetime import datetime
+
+subreddit_name = 'UCSantaBarbara'
 
 reddit = praw.Reddit(client_id=mySecrets['client_id'], client_secret=mySecrets['client_secret'], user_agent=mySecrets['user_agent'])
 
-def hot():
-    subreddit = reddit.subreddit('UCSantaBarbara').hot(limit=5)
+def name(subreddit_name):
+    subreddit = reddit.subreddit(subreddit_name)
+    return subreddit.display_name
+
+def description(subreddit_name):
+    subreddit = reddit.subreddit(subreddit_name)
+    return subreddit.public_description
+
+def num_subscribers(subreddit_name):
+    subreddit = reddit.subreddit(subreddit_name)
+    return subreddit.subscribers
+
+def time_created(subreddit_name):
+    subreddit = reddit.subreddit(subreddit_name)
+    parsed_date = datetime.utcfromtimestamp(subreddit.created_utc)
+    year = parsed_date.year
+    month = parsed_date.month
+    day = parsed_date.day
+    return str(month) + '.' + str(day) + '.' + str(year)
+
+def hot(subreddit_name):
+    subreddit = reddit.subreddit(subreddit_name).hot(limit=3)
 
     # make dataframe with data
     hot_df = pd.DataFrame(columns = ['title', 'url'])
@@ -17,6 +40,61 @@ def hot():
         }, ignore_index=True)
 
     return hot_df
+
+def new(subreddit_name):
+    subreddit = reddit.subreddit(subreddit_name).new(limit=3)
+
+    # make dataframe with data
+    new_df = pd.DataFrame(columns = ['title', 'url'])
+    for post in subreddit:
+        new_df = new_df.append({
+            'title': post.title,
+            'url': 'https://www.reddit.com' + post.permalink
+        }, ignore_index=True)
+
+    return new_df
+
+def top_users(subreddit_name, time_filter):
+    subreddit = reddit.subreddit(subreddit_name).top(time_filter = time_filter)
+
+    user_dict = {}
+    for post in subreddit:
+        # add score for author
+        if post.author.name in user_dict:
+            user_dict[post.author.name] += 1
+        else:
+            user_dict[post.author.name] = 1
+
+        # add score for each commentor
+        for comment in post.comments:
+            if comment.author.name in user_dict:
+                user_dict[comment.author.name] += 1
+            else:
+                user_dict[comment.author.name] = 1
+
+    # sort so that top users are first
+    # {k: v for k, v in sorted(user_dict.items(), key=lambda item: item[1])}
+    user_list = sorted(user_dict, key=user_dict.__getitem__, reverse=True)
+
+    # make dataframe with data
+    user_df = pd.DataFrame(columns = ['place', 'username'])
+    i = 1
+    for user in user_list:
+        if i < 6:
+            user_df = user_df.append({
+                'place': i,
+                'username': user_list[i - 1]
+            }, ignore_index=True)
+        else:
+            break
+        i += 1
+
+    return user_df
+
+
+
+
+
 
 
 
