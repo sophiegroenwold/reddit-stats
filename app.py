03453f497b2cef4df5cec0d148345203b51a131e
sophiegroenwold <sophiegroenwold@umail.ucsb.edu>
2021-02-06 12:17:20 -0800

    following dash tutorial for heroku

--- app.py
+++ app.py
@@ -11,9 +11,10 @@ import pandas as pd
 import data
 import toxicity
 
-# external_stylesheets = ['https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap']
+external_stylesheets = ['https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap']
 
-# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 def generate_table(dataframe, max_rows=10):
     return html.Table([
@@ -147,5 +148,5 @@ def update_output(input):
             )
         ])
 
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- manage.py
+++
@@ -1,7 +0,0 @@
-import dash
-
-external_stylesheets = ['https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
